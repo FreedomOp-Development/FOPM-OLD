61e77a1cd38f3ebd377a92720dfcbe571ddc5132
Brandon <kellatech38@gmail.com>
2016-07-16 21:37:31 +0100

    test for eclipse link LOOL

--- src/me/buildcarter8/FreedomOpMod/FOPM_Messages.java
+++ src/me/buildcarter8/FreedomOpMod/FOPM_Messages.java
@@ -1,10 +1,11 @@
+// test lol 
 package me.buildcarter8.FreedomOpMod;
 
 import org.bukkit.ChatColor;
 
 public class FOPM_Messages
 {
-
+    public static final String ANNOUNCEMENT = ChatColor.BLUE + "Announcement >";
     public static final String MOD_TAG = ChatColor.BLUE + "FreedomOpMod > " + ChatColor.GRAY;
     public static final String MSG_NO_PERMS = FOPM_Messages.MOD_TAG + ChatColor.YELLOW + "You do not have the required permissions for this command";
 }
