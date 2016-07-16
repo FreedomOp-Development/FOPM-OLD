70382a52c05ec61edeaeffb0fd1854ca74ca3f25
Liam Townsley <DUFCLiam@users.noreply.github.com>
2016-07-16 21:10:18 +0100

    Adding more messages.

--- src/me/buildcarter8/FreedomOpMod/FOPM_Messages.java
+++ src/me/buildcarter8/FreedomOpMod/FOPM_Messages.java
@@ -7,4 +7,8 @@ public class FOPM_Messages
 
     public static final String MOD_TAG = ChatColor.BLUE + "FreedomOpMod > " + ChatColor.GRAY;
     public static final String MSG_NO_PERMS = FOPM_Messages.MOD_TAG + ChatColor.YELLOW + "You do not have the required permissions for this command";
+    public static final String MSG_NOT_OP = FOPR_Messages.MOD_TAG + ChatColor.YELLOW + "You are no longer OP!";
+    public static final String MSG_NOW_OP = FOPR_Messages.MOD_TAG + ChatColor.YELLOW + "You are now OP!";
+    public static final String NO_PERMS = FOPR_Messages.MOD_TAG + ChatColor.RED + "You do not have the required permissions to use this command!";
+    
 }
