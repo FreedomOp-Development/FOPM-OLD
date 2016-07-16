728b4f5948f7465580e714eff7e8f136fdfd2cdd
buildcarter8 <b68685@mvrht.com>
2016-07-16 17:39:37 -0400

    ugh

--- src/me/buildcarter8/FreedomOpMod/Main.java
+++ src/me/buildcarter8/FreedomOpMod/Main.java
@@ -1,5 +1,6 @@
 package me.buildcarter8.FreedomOpMod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.bukkit.Bukkit;
@@ -11,14 +12,16 @@ import me.buildcarter8.FreedomOpMod.Listeners.*;
 public class Main extends JavaPlugin
 {
     //
-    public boolean debug = true;
     public static String VERSION = "0.1";
     public static String plugin_file = "plugin.yml";
     public static Main plugin;
     public static final String SUPERADMIN_FILE = "superadmin.yml";
     public static final String CONFIG_FILE = "config.yml";
     public static String NOPE = ChatColor.RED + "You do not have the correct rank to use this command";
+    public static final String COMMAND_PATH = "me.buildcarter8.FreedomOpMod.Commands";
+    public static final String COMMAND_PREFIX = "Command_";
     public static final List<String> DEVELOPERS = Arrays.asList("buildcarter8", "tylerhyperHD", "Cyro1999");
+   // private static List<String> seniorAdminNames = new ArrayList<String>();
     //
     public FOPM_PluginLog log;
     public FOPM_CommandLoader cl;
@@ -49,25 +52,28 @@ public class Main extends JavaPlugin
         FOPM_PluginLog.info("FOPM has been disabled");
     }
 
-    public boolean areWeDebugging() {
-        return debug;
-    }
-    
     public void loadMainConfig()
     {
 
     }
 
+    public static List<String> superadmins = new ArrayList<String>();
+    public static List<String> superadmin_ips = new ArrayList<String>();
+
     public void loadSuperadminConfig()
     {
         try
         {
-            FOPM_AdministratorList.backupSavedList();
-            FOPM_AdministratorList.loadSuperadminList();
+            FOPM_SuperadminList.backupSavedList();
+            FOPM_SuperadminList.loadSuperadminList();
+
+            superadmins = FOPM_SuperadminList.getSuperadminNames();
+            superadmin_ips = FOPM_SuperadminList.getSuperadminIPs();
         }
         catch (Exception ex)
         {
             FOPM_PluginLog.severe("Error loading superadmin list: " + ex.getMessage());
         }
     }
+
 }
